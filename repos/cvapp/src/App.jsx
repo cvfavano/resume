b7a8c9696180c9e689fbcbbb1c2c6d4c6c84ddca
Christa Favano <crstav@gmail.com>
2024-09-07 23:35:14 -0400

    removed param in handler

--- repos/cvapp/src/App.jsx
+++ repos/cvapp/src/App.jsx
@@ -28,24 +28,24 @@ function App() {
     endDate: '2005-02-15',
     title: 'Consultant',
     details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }
-])
-const [workForm, setWorkForm] = useState({
-  id: '6482d9c8-46e9-506d-3a5a-53389eb4a7c7',
-  company:'AGK Consulting',
-  startDate: '2000-01-22',
-  endDate: '2005-02-15',
-  title: 'Consultant',
-  details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-})
+  }])
+
+  const [workForm, setWorkForm] = useState({
+    id: '6482d9c8-46e9-506d-3a5a-53389eb4a7c7',
+    company:'AGK Consulting',
+    startDate: '2000-01-22',
+    endDate: '2005-02-15',
+    title: 'Consultant',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  })
 
 
-const [education, setEducation] = useState({
-  name:'ACS University',
-  major: 'BSc Chemical Engineering',
-  location:'London, UK',
-  date: '2006-12-12'
-})
+  const [education, setEducation] = useState({
+    name:'ACS University',
+    major: 'BSc Chemical Engineering',
+    location:'London, UK',
+    date: '2006-12-12'
+  })
 
 
 const handlePersonalChange = (event) => {
@@ -58,54 +58,48 @@ const handlePersonalChange = (event) => {
   }))
 }
 
-const handleDelete = (id) => {
+  const handleDelete = (id) => {
 
-  let filteredExperience = experience.filter((job => job.id !== id ));
-  
-  setExperience(filteredExperience)  
-  setWorkForm(filteredExperience.at(-1))
-}
-
-if(experience.length === 0 ){
-  const newExperience = {
-    id: uuid(),
-    company:'Add Company',
-    startDate: '',
-    endDate: '',
-    title: 'Add Title',
-    details: 'Add Description'
+    let filteredExperience = experience.filter((job => job.id !== id ));
+    
+    setExperience(filteredExperience)  
+    setWorkForm(filteredExperience.at(-1))
   }
-  setExperience([newExperience])  
-  setWorkForm(newExperience)
-}
+
+  if(experience.length === 0 ){
+    const newExperience = {
+      id: uuid(),
+      company:'Add Company',
+      startDate: '',
+      endDate: '',
+      title: 'Add Title',
+      details: 'Add Description'
+    }
+    setExperience([newExperience])  
+    setWorkForm(newExperience)
+  }
 
   const handleUpdateWorkHistoryForm = (event, id) => {
 
     const {name, value} = event.target;
-      const updateExperience = experience.map(job => {
-        if (job.id  === id) 
-         return( {...job, [name]: value});
-        
-        else 
-         return job;
-        })
-        
-        setExperience(updateExperience);
-     
-        setWorkForm(prevWorkForm => ({
-            ...prevWorkForm, 
-            [name]: value
-            })
-          )
-          
-        
+    const updateExperience = experience.map(job => {
+      if (job.id  === id) 
+        return( {...job, [name]: value});
       
-    //  console.log(updateExperience)
- //     setWorkForm(updateWorkForm);
-    //  console.log(workForm)
+      else 
+        return job;
+      })
+        
+      setExperience(updateExperience);
+    
+      setWorkForm(prevWorkForm => ({
+        ...prevWorkForm, 
+        [name]: value
+        })
+      )         
     }
 
-    //edit button trigger //update state of experience and workForm
+ 
    
   
   const handleCreateWorkHistory = () => {
@@ -143,28 +137,25 @@ if(experience.length === 0 ){
       <div id='sidebar'>
         <PersonalInfoInput 
           props =  { personalInfo }
-          changeHandler = { () => {handlePersonalChange(event)} }
-        /> 
+          changeHandler = { handlePersonalChange }
+        /> gita 
         
-      <div>
+      
         <WorkExperienceForm
           details = { workForm }
           onChangeHandler = {handleUpdateWorkHistoryForm }
           addHandler = { handleCreateWorkHistory } />
-      </div>
+   
   
-      <Education 
-        props = {education}
-        changeHandler={ () => {handlePersonalChange(event, setEducation)} }
-      />
+        <Education 
+          props = {education}
+          changeHandler={ () => {handlePersonalChange(event, setEducation)} }
+        />
 
       </div>
     
       <div id='main'>
-     
-        <PersonalDetails details = { personalInfo } /> 
-
-      
+        <PersonalDetails details = { personalInfo } />       
         <div>
           <h2 className='header'>Work Experience</h2>
           <WorkExperienceDetails 
@@ -175,11 +166,7 @@ if(experience.length === 0 ){
         </div>
         
         <EducationDetails details = { education } />
-       
-        
-    
       </div>
-
     </div>
   )
 }
